1c7ca4e67e32418b28e629920b68453e550ce0ee
Flavia R. Paião <flarpaiao.home@gmail.com>
2024-02-20 16:05:43 -0300

    interface

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,19 +1,18 @@
-//import React from 'react';
 import './Home.css';
 
 
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-teal-700 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+              <p className='text-xl'>Compre aqui</p>
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</button>
               </div>
             </div>
   
