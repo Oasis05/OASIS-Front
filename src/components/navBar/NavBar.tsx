83a50d50b43ba8da1560b35662bf6e60f7b6d471
Anderson <ander_son12@hotmail.com>
2024-02-20 15:13:56 -0300

    rotas

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,4 @@
-//import React from 'react'
-
+import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar() {
  
@@ -9,9 +8,11 @@ function Navbar() {
     <>
      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Projeto OASIS</div>
+            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
 
             <div className='flex gap-4'>
+              <Link to='/login' className='hover:underline'>Login</Link>
+              <Link to='/home' className='hover:underline'>Home</Link>
               <div className='hover:underline'>Postagens</div>
               <div className='hover:underline'>Temas</div>
               <div className='hover:underline'>Cadastrar tema</div>
