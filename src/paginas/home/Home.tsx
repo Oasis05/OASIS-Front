384f9a28957db352cb43946cb5591957d47e90bf
Anderson <ander_son12@hotmail.com>
2024-02-23 11:23:15 -0300

    criando models

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,18 +1,11 @@
 import './Home.css';
-import React, { useContext } from 'react';
-import { UserContext } from '../../contexts/AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
 
-function Home() {
-  useContext(UserContext);
-  const estiloDoFundo = {
-    backgroundImage: `url('https://i.imgur.com/xvzLDxo.jpg')`,
-    backgroundSize: '100%',
-    height: 400
-  };
+function Home() { 
 
   return (
     <>
-      <div className="flex justify-center items-center" style={estiloDoFundo}>
+      <div className="flex justify-center items-center estiloDoFundo"  >
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
