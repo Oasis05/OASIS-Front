07aab29184d913993f2844c1119b605f0ceb978d
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-29 14:51:02 -0300

    categorias delete

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios";
 
 const api = axios.create({
@@ -9,26 +10,40 @@ export const cadastrarUsuario = async(url: string, dados: Object, setDados: Func
   setDados(resposta.data)
 }
 
-export const login = async(url: string, dados: Object, setDados: Function) => {
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data)
-}
+export const login = async (url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
 
-export const buscar = async(url: string, setDados: Function, header: Object) => {
-  const resposta = await api.get(url, header)
-  setDados(resposta.data)
-}
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
 
-export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.post(url, dados, header)
-  setDados(resposta.data)
-}
+export const cadastrar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
 
-export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.put(url, dados, header)
-  setDados(resposta.data)
-}
+export const atualizar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
 
-export const deletar = async(url: string, header: Object) => {
-  await api.delete(url, header)
-}
+export const deletar = async (url: string, header: Object) => {
+  await api.delete(url, header);
+};
