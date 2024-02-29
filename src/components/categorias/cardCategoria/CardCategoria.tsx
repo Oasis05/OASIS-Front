1c873a7c94f4e3a905620b0d059e611d8af76195
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-29 14:24:54 -0300

    categorias e appTsx rotas

---
+++ src/components/categorias/cardCategoria/CardCategoria.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import Categoria from '../../../models/Categorias'
+
+interface CardCategoriaProps {
+  categoria: Categoria
+}
+
+function CardCategorias({categoria}: CardCategoriaProps) {
+  return (
+    
+  )
+}
+
+export default CardCategorias
---
+++ src/components/categorias/deletarCategoria/DeletarCategoria.tsx
@@ -0,0 +1,13 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { AuthContext } from "../../../contexts/AuthContext";
+import Categoria from "../../../models/Categorias";
+import { buscar, deletar } from "../../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
+
+function DeletarCategoria() {
+	
+	
+}
+
+export default DeletarCategoria;
