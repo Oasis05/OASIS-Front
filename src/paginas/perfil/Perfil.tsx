3ee1137be7a15c12dcc50e0badebe4d1828eda3b
Flavia R. Paião <flarpaiao.home@gmail.com>
2024-03-01 16:25:30 -0300

    Atualização perfil

--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../contexts/AuthContext'
 
 function Perfil() {
 
-  let navigate = useNavigate()
+    let navigate = useNavigate();
 
     const { usuario } = useContext(AuthContext)
 
@@ -13,18 +13,22 @@ function Perfil() {
             alert('Você precisa estar logado')
             navigate("/login")
         }
-    }, [usuario.token])
-    
-  return (
-    <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
-      <img className='w-full h-72 object-cover border-b-8 border-white' src="https://i.imgur.com/x73G4P6.jpg" alt="Capa do Perfil" />
-      <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-gradient-to-t from-lime-900 text-white text-2xl items-center justify-center">
-        <p>Nome: {usuario.nome} </p>
-        <p>Email: {usuario.usuario}</p>
-      </div>
-    </div>
-  )
+    }, [usuario.token]);
+
+    return (
+        <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
+            <img className='w-full h-72 object-cover border-b-8 border-white' src="https://i.imgur.com/x73G4P6.jpg" alt="Capa do Perfil" />
+            {usuario.foto === " " ? <img src="https://i.imgur.com/bbC0sF0.jpg" alt="Foto padrão"
+                className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
+                :
+                <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
+            }
+            <div className="relative mt-[-6rem] h-72 flex flex-col bg-gradient-to-t from-lime-900 text-white text-2xl items-center justify-center">
+                <p>Nome: {usuario.nome} </p>
+                <p>Email: {usuario.usuario}</p>
+            </div>
+        </div>
+    );
 }
 
-export default Perfil
+export default Perfil;
