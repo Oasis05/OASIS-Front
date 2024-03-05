69a0a04b0d3d63c0c2c5c9dd62bf11032c298761
guilhermerubrae <guilhermerubrae@gmail.com>
2024-03-05 14:32:42 -0300

    up cor

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
 
   return (
     <>
-      <div className="text-lime-950" style={{ backgroundColor: 'rgba(33, 59, 28, 0.71)'}}>
+      <div className="text-lime-950" style={{ backgroundColor: 'rgba(178, 205, 172, 0.71)'}}>
         <div className="container flex justify-between items-center py-4 px-8">
           <Link to="/home" className="text-lime-950">
             <img src={Logo} className="w-14 sm:w-14 ml-40" alt="Logo" />
