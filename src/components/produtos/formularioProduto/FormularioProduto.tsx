89e6facf462e4bc54e03742769b2d8e40735837b
Anderson <ander_son12@hotmail.com>
2024-03-05 16:58:17 -0300

    Mudança nome button cadastrar produto

--- src/components/produtos/formularioProduto/FormularioProduto.tsx
+++ src/components/produtos/formularioProduto/FormularioProduto.tsx
@@ -213,7 +213,7 @@ function FormularioProduto() {
           </select>
         </div>
         <button type='submit' className='mt-4 bg-lime-800 py-2 px-4 text-white rounded-md hover:bg-lime-700 focus:outline-none'>
-          Editar
+          Cadastrar
         </button>
       </form>
     </div>
