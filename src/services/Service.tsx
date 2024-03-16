910943ca215ceaff29031e0387ba634758e037dd
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-03-16 17:15:32 -0300

    altera para localhost

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:8080"
+  //import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
