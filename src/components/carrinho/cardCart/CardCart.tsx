e1030bda89a43f08e5ce1685c02c846cdda86140
guilhermerubrae <guilhermerubrae@gmail.com>
2024-03-20 05:03:18 -0300

    teste gui carrinho estilização 17

--- src/components/carrinho/cardCart/CardCart.tsx
+++ src/components/carrinho/cardCart/CardCart.tsx
@@ -64,7 +64,7 @@ function CardCart({ item }: CardProdutosProps) {
         
         <div>
       <button onClick={toggleOptions}>Qauntidade</button>
-      {showOptions && (
+
         <select
           aria-label="Select quantity"
           className="text-sm mb-3 absolute top-0 right-0 mt-8 bg-white rounded-lg p-2 py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
@@ -79,32 +79,7 @@ function CardCart({ item }: CardProdutosProps) {
           <option>08</option>
           <option>09</option>
         </select>
-      )}
     </div>
-
-        {usuario.tipo === "1" ? (
-          <button
-            onClick={toggleOptions}
-            className="absolute top-0 right-0 mt-3 mr-3 p-2 focus:outline-none z-10"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-gray-800 hover:text-gray-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        ) : (
-          <div></div>
-        )}
       </div>
     </div>
   );
