8aad287d30114017fa5ca3add766199b38a73a10
Anderson <ander_son12@hotmail.com>
2024-03-19 22:10:48 -0300

    mudança rota API

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "http://localhost:8080"
-  //'https://oasis-vx1q.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URL
+  
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
